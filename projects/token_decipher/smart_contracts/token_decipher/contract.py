257b54395716c6101dd326d72e5f2c7e4f517853
Camilo Molano <camilo@algorand.foundation>
2024-04-22 15:03:44 -0500

    Create token

--- projects/token_decipher/smart_contracts/token_decipher/contract.py
+++ projects/token_decipher/smart_contracts/token_decipher/contract.py
@@ -1,7 +1,27 @@
-from algopy import ARC4Contract, arc4
+from algopy import ARC4Contract, String, arc4, itxn
 
 
 class TokenDecipher(ARC4Contract):
+
+    asset_name: String
+    unit_name: String
+
+    @arc4.abimethod(allow_actions=["NoOp"],create="require")
+    def createApplication(self, assetName:String, uintName:String) -> None:     # noqa: N802, N803
+        self.asset_name = assetName
+        self.uint_name = uintName
+
     @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+    def createAsset(self) ->None:                                                 # noqa: N802
+        self.asset_created = (
+            itxn.AssetConfig(
+            asset_name= self.asset_name,
+            unit_name=self.unit_name,
+            total= 1000,
+            decimals= 1,
+            default_frozen=False
+        )
+        .submit()
+        .created_asset.id
+        )
+
